df4e1063efdc94879175b877dfc3f66f4533aa32
wickerdj <wickerdj@gmail.com>
2025-10-19 20:08:46 +0000

    working on debug

--- build.zig
+++ build.zig
@@ -12,7 +12,8 @@ pub fn build(b: *std.Build) void {
     const firmware = mb.add_firmware(.{
         .name = "blinky",
         .target = mb.ports.rp2xxx.boards.raspberrypi.pico,
-        .optimize = .ReleaseSmall,
+        // .optimize = .ReleaseSmall, // For release
+        .optimize = .Debug, // For debugging
         .root_source_file = b.path("src/main.zig"),
     });
 
